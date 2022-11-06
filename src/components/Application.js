d2f2cd00cda6ada60bcbfa2d90ef5a4ea35519a3
JakobBollman <jakob@bollman.ca>
2022-11-05 18:38:47 -0600

    appointments by day

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,71 +3,35 @@ import axios from "axios";
 import "components/Application.scss";
 import  DayList from "components/DayList";
 import Appointment from "./Appointment/index";
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-const appointItems = Object.values(appointments).map(appData => {
-  return (
-    <Appointment
-      key={appData.id}
-      time={appData.time}
-      interview={appData.interview}
-    />
-  )
-})
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
+  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
-
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));
-  }
-
+  const appointItems = dailyAppointments.map(appData => {
+    return (
+      <Appointment
+        key={appData.id}
+        time={appData.time}
+        interview={appData.interview}
+      />
+    )
+  })
   useEffect(() => {
-    axios.get("/api/days").then(response => setDays(response.data));
-  }, []);
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    });
+  })
 
   return (
     <main className="layout">
