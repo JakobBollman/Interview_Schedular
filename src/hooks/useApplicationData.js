b4d5073bf90cd2b5ad18bafd816274f989e65d3a
JakobBollman <jakob@bollman.ca>
2022-11-20 11:44:55 -0700

    Moved useEffect to top of page

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,6 +11,16 @@ export default function useApplicationData(props) {
 
   const setDay = day => setState({ ...state, day });
 
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    });
+  },[]);
+
   function calcSpots(state, appointments){
     return state.days.map((day) => {
       if(day.name === state.day) {
@@ -53,16 +63,6 @@ export default function useApplicationData(props) {
       .then(setState(prev => ({...prev, appointments, days: calcSpots(state, appointments) 
     })))
   }
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
-  },[]);
 
   return { state, setDay, bookInterview, cancelInterview }
 }
